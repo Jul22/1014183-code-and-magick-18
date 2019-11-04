4000d66950b3fae839160e1a004dc509e433ee8e
Jul22 <gerasimovayuliya88@gmail.com>
2019-11-04 15:01:53 +0300

    adding backend module

---
+++ js/backend.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(function () {
+  var OK_STATUS = 200;
+  var URL_GET = 'https://js.dump.academy/code-and-magick/data';
+  var URL_POST = 'https://js.dump.academy/code-and-magick';
+  var save = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === OK_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('POST', URL_POST);
+    xhr.send(data);
+  };
+
+  var load = function (onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.addEventListener('load', function () {
+      if (xhr.status === OK_STATUS) {
+        onLoad(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
+
+    xhr.timeout = 10000;
+
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+
+  window.backend = {
+    save: save,
+    load: load
+  };
+})();
