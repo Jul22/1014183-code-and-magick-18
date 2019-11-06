7bee96d5bf997e804e1ebb0b4e62a26c0156d2db
Jul22 <gerasimovayuliya88@gmail.com>
2019-11-07 01:22:54 +0300

    fix for Travis

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,6 @@
   var WIZARD_COATS_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-  var WIZARDS_AMOUNT = 4;
   var userDialog = document.querySelector('.setup');// Choosing wizard window
   var similarListElement = document.querySelector('.setup-similar-list');// Characters list
   var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
