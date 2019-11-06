a5f4694e4dfea664ef9688e759fef54497dc9317
Jul22 <gerasimovayuliya88@gmail.com>
2019-11-07 01:10:17 +0300

    pattern DRY

--- js/backend.js
+++ js/backend.js
@@ -2,33 +2,24 @@
 
 (function () {
   var OK_STATUS = 200;
+  var TIME_OUT = 10000;
   var URL_GET = 'https://js.dump.academy/code-and-magick/data';
   var URL_POST = 'https://js.dump.academy/code-and-magick';
+
   var save = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === OK_STATUS) {
-        onLoad(xhr.response);
-      } else {
-        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000;
-
+    var xhr = xhrSetup(onLoad, onError);
     xhr.open('POST', URL_POST);
     xhr.send(data);
   };
 
   var load = function (onLoad, onError) {
+    var xhr = xhrSetup(onLoad, onError);
+    xhr.open('GET', URL_GET);
+    xhr.send();
+  };
+
+  // DRY pattern
+  var xhrSetup = function (onLoad, onError) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -45,11 +36,8 @@
     xhr.addEventListener('timeout', function () {
       onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
     });
-
-    xhr.timeout = 10000;
-
-    xhr.open('GET', URL_GET);
-    xhr.send();
+    xhr.timeout = TIME_OUT;
+    return xhr;
   };
 
   window.backend = {
